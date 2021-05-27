501c50d44987bad38b396c9cd5db6eec72014b46
Cynthia Fu <yinnycfu@gmail.com>
2021-05-27 16:12:55 +0100

    test for no bikes in station

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,13 +2,23 @@ require_relative 'bike'
 
 class DockingStation
   attr_reader :bike
+  
+  def initialize
+    @bike = []
+  end
 
   def release_bike
-   Bike.new
+    # Bike.new
+    if @bike.empty?
+      raise "No more bikes"
+    else
+      @bike.pop    
+    end
   end
 
   def dock(bike)
-    @bike = bike
+    @bike << bike
+    bike
   end
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,6 +4,7 @@ describe DockingStation do
   it { is_expected.to respond_to :release_bike }
 
   it 'releases the working bike' do
+    subject.dock(Bike.new) # dummy bike in the station
     bike = subject.release_bike
     expect(bike).to be_working
   end
@@ -21,8 +22,14 @@ describe DockingStation do
     bike = Bike.new
     subject.dock(bike)
 
-    expect(subject.bike).to eq bike
+    expect(subject.bike).to eq [bike]
   end
+
+  it 'raises an error if there are no bikes in the station' do
+    expect{subject.release_bike}.to raise_error('No more bikes')
+  end  
+
+
 
 end
 
