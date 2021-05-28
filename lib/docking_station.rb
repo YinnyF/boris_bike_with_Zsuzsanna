de6ab5da7f61d8917f4534fe476b24cb9521c6dd
Zsuzsanna Ver <56695526+MrsVer@users.noreply.github.com>
2021-05-28 15:51:54 +0100

    Create bike_shed to store more than one bike

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,17 +1,20 @@
 require_relative 'bike'
 
 class DockingStation
-  attr_reader :bike
+  attr_reader :bike_shed
   
+  def initialize
+    @bike_shed = []
+  end
+
   def release_bike
-    # Bike.new
-    raise "No more bikes" unless @bike
-    @bike
+    raise "No more bikes in the bike shed" if @bike_shed.empty?
+    @bike_shed.pop
   end
 
   def dock(bike)
-    raise "Dock is full" unless !@bike
-    @bike = bike
+    raise "Dock is full" if !@bike_shed.empty?
+    @bike_shed << bike
   end
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -11,29 +11,32 @@ describe DockingStation do
 
   it { is_expected.to respond_to(:dock).with(1).argument }
 
-  it { is_expected.to respond_to(:bike) }
+  it { is_expected.to respond_to(:bike_shed) }
 
   it 'docks something' do
     bike = Bike.new
     expect(subject.dock(bike)).to eq (bike)
   end
 
-  it 'returns docked bikes' do
+  it 'returns docked bike' do
     bike = Bike.new
     subject.dock(bike)
 
-    expect(subject.bike).to eq bike
+    expect(subject.bike_shed).to eq bike
   end
 
   it 'raises an error if there are no bikes in the station' do
-    expect { subject.release_bike } .to raise_error('No more bikes')
+    expect { subject.release_bike } .to raise_error('No more bikes in the bike shed')
   end  
 
-  it 'to raise an error if you try to dock a bike when the dock is full' do
+  it 'to not raise an error if you try to dock a bike when the dock is not full' do
     bike = Bike.new
-    subject.dock(bike)
-    bike2 = Bike.new
-    expect { subject.dock(bike2) } .to raise_error('Dock is full')
+    expect { subject.dock(bike) } .not_to raise_error('Dock is full')
+  end
+
+  it 'error message when tring to dock 20 bike' do
+    20.times { subject.dock Bike.new }
+    expect { subject.dock Bike.new } .to raise_error('Dock is full')
   end
 
 
